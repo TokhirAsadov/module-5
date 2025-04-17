ec63beb3925c9440523d204e7ec17cc1ab8458b2
Takhir <guvalakat1603@gmail.com>
2025-04-17 12:01:20 +0500

    FazliddinExample

---
+++ lesson_5/src/main/java/uz/javatuz/FazliddinExample.java
@@ -0,0 +1,49 @@
+package uz.javatuz;
+
+import uz.javatuz.entity.User;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FazliddinExample {
+    public static void main(String[] args) {
+
+
+
+//        Set<User> users = List.of(
+//                        "Murodjon",
+//                        "ALi",
+//                        "Vali"
+//                ).stream()
+//                .map(str -> new User(str))
+//                .collect(Collectors.toSet());
+//        System.out.println(users);
+
+
+//        Stream<>
+//        Stream.of(
+//
+//        )
+        List<Integer> integers = List.of(
+                1, 2, 3, 4, 5
+        );
+
+//        integers.stream().forEach(System.out::println);
+//        Stream<Integer> integerStream = integers.stream()
+//                .filter(i -> i % 2 == 0);
+//        integerStream.forEach(System.out::println);
+//
+//        Stream<Integer> integerStream1 = integers.stream()
+//                .map(integer -> integer * integer);
+//        integerStream1.forEach(System.out::println);
+
+
+
+//        for (Integer integer : integers) {
+//            System.out.println(integer);
+//        }
+    }
+
+}
